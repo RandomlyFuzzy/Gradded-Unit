ca44a06b15de05099cc3d99c11d0f747c264a6d6
RandomlyFuzzy <liam-woolley1@hotmail.com>
2019-02-27 12:47:05 +0000

    nothing

--- Gradded-Unit/src/com/FuturePixels/MainClasses/Utils/CalcUtils.java
+++ Gradded-Unit/src/com/FuturePixels/MainClasses/Utils/CalcUtils.java
@@ -1,48 +1,14 @@
-<<<<<<< HEAD:Gradded-Unit/src/Entry.java
-
-import com.FuturePixels.Engine.AbstractClasses.ILevel;
-import com.FuturePixels.Engine.Entry.Game;
-import com.FuturePixels.Engine.Utils.LevelLoader;
-import com.FuturePixels.levels.Menus.*;
-import com.FuturePixels.levels.OtherLevels.*;
-import com.FuturePixels.levels.SoloLevels.*;
-import java.util.ArrayList;
-
-=======
->>>>>>> parent of aa3bd39... Added music and made engine modular:Gradded-Unit/src/com/FuturePixels/MainClasses/Utils/CalcUtils.java
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-<<<<<<< HEAD:Gradded-Unit/src/Entry.java
-=======
 package com.FuturePixels.MainClasses.Utils;
 
->>>>>>> parent of aa3bd39... Added music and made engine modular:Gradded-Unit/src/com/FuturePixels/MainClasses/Utils/CalcUtils.java
 /**
  *
  * @author RandomlyFuzzy
  */
-<<<<<<< HEAD:Gradded-Unit/src/Entry.java
-public class Entry {
-
-    public static void main(String args[]) {
-        Game.init(new MainMenu());
-        ArrayList<ILevel> Levels = new ArrayList<ILevel>();
-        Levels.add(new MainMenu());
-        Levels.add(new Settings());
-        Levels.add(new LeaderBoard());
-        Levels.add(new Level1Solo());
-        Levels.add(new Level2Solo());
-        Levels.add(new Level3Solo());
-        Levels.add(new LevelSelect());
-
-        com.FuturePixels.Engine.Utils.LevelLoader.setLEVELS(Levels);
-
-    }
-=======
 public class CalcUtils {
     
->>>>>>> parent of aa3bd39... Added music and made engine modular:Gradded-Unit/src/com/FuturePixels/MainClasses/Utils/CalcUtils.java
 }
